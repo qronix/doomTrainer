1764ab21935200b6f81737ab3eae0a3d992b0c94
qronix <qroniix@gmail.com>
2019-02-28 19:00:29 -0500

    Submission to GitHub

---
+++ doomTrainerNew/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace doomTrainerNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAmmo = new System.Windows.Forms.Button();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.lblAmmoStatus = new System.Windows.Forms.Label();
+            this.lblGameStatus = new System.Windows.Forms.Label();
+            this.lblHealthStatus = new System.Windows.Forms.Label();
+            this.btnHealth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAmmo
+            // 
+            this.btnAmmo.Location = new System.Drawing.Point(12, 98);
+            this.btnAmmo.Name = "btnAmmo";
+            this.btnAmmo.Size = new System.Drawing.Size(75, 23);
+            this.btnAmmo.TabIndex = 0;
+            this.btnAmmo.Text = "Ammo";
+            this.btnAmmo.UseVisualStyleBackColor = true;
+            this.btnAmmo.Click += new System.EventHandler(this.btnAmmo_Click);
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblAmmoStatus
+            // 
+            this.lblAmmoStatus.AutoSize = true;
+            this.lblAmmoStatus.Location = new System.Drawing.Point(12, 68);
+            this.lblAmmoStatus.Name = "lblAmmoStatus";
+            this.lblAmmoStatus.Size = new System.Drawing.Size(56, 13);
+            this.lblAmmoStatus.TabIndex = 1;
+            this.lblAmmoStatus.Text = "Ammo: Off";
+            // 
+            // lblGameStatus
+            // 
+            this.lblGameStatus.AutoSize = true;
+            this.lblGameStatus.Location = new System.Drawing.Point(12, 30);
+            this.lblGameStatus.Name = "lblGameStatus";
+            this.lblGameStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblGameStatus.TabIndex = 2;
+            this.lblGameStatus.Text = "Status:";
+            // 
+            // lblHealthStatus
+            // 
+            this.lblHealthStatus.AutoSize = true;
+            this.lblHealthStatus.Location = new System.Drawing.Point(138, 68);
+            this.lblHealthStatus.Name = "lblHealthStatus";
+            this.lblHealthStatus.Size = new System.Drawing.Size(58, 13);
+            this.lblHealthStatus.TabIndex = 3;
+            this.lblHealthStatus.Text = "Health: Off";
+            // 
+            // btnHealth
+            // 
+            this.btnHealth.Location = new System.Drawing.Point(141, 98);
+            this.btnHealth.Name = "btnHealth";
+            this.btnHealth.Size = new System.Drawing.Size(75, 23);
+            this.btnHealth.TabIndex = 4;
+            this.btnHealth.Text = "Health";
+            this.btnHealth.UseVisualStyleBackColor = true;
+            this.btnHealth.Click += new System.EventHandler(this.btnHealth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnHealth);
+            this.Controls.Add(this.lblHealthStatus);
+            this.Controls.Add(this.lblGameStatus);
+            this.Controls.Add(this.lblAmmoStatus);
+            this.Controls.Add(this.btnAmmo);
+            this.Name = "Form1";
+            this.Text = "VirtualCopTrainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAmmo;
+        private System.Windows.Forms.Timer timerUpdate;
+        private System.Windows.Forms.Label lblAmmoStatus;
+        private System.Windows.Forms.Label lblGameStatus;
+        private System.Windows.Forms.Label lblHealthStatus;
+        private System.Windows.Forms.Button btnHealth;
+    }
+}
+
---
+++ doomTrainerNew/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Threading;
+using Gma.System.MouseKeyHook;
+
+
+namespace doomTrainerNew
+{
+    public partial class Form1 : Form
+    {
+
+        Gma.System.MouseKeyHook.Hook
+
+        #region Global variables
+        Memory myMemory = new Memory();
+        Process[] myProcess;
+        bool isGameAvailable = false;
+
+        bool unlimitedAmmo = false;
+        string AmmoPointer = "004CF1F0";
+        int[] AmmoOffset = {0x28};
+        int AmmoToFill = 6;
+
+        bool unlimitedHealth = false;
+        string HealthPointer = "004CF1E6"; //address is static may need to use just memory read not ptrRead/Write
+        int[] HealthOffset = { 0x0 };
+        int HealthToSet = 10;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerUpdate.Start();
+        }
+
+        private void btnAmmo_Click(object sender, EventArgs e)
+        {
+            if (isGameAvailable)
+            {
+                if (!unlimitedAmmo)
+                {
+                    unlimitedAmmo = true;
+                    lblAmmoStatus.Text = "Ammo: On";
+                }
+                else if (unlimitedAmmo)
+                {
+                    unlimitedAmmo = false;
+                    lblAmmoStatus.Text = "Ammo: Off";
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            myProcess = Process.GetProcessesByName("VirtualCop2");
+            if(myProcess.Length == 1)
+            {
+                isGameAvailable = true;
+                if(lblGameStatus.Text != "Process found!")
+                {
+                    lblGameStatus.Text = "Process found!";
+                }
+                if (unlimitedAmmo)
+                {
+                    myMemory.ReadProcess = myProcess[0];
+                    myMemory.Open();
+                    int pointerAddy = HexToDec(AmmoPointer);
+                    int[] pointerOffset = AmmoOffset;
+                    int bytesWritten;
+                    byte[] valueToWrite = BitConverter.GetBytes(AmmoToFill);
+                    string writtenAddress = myMemory.PointerWrite((IntPtr)pointerAddy, valueToWrite, pointerOffset, out bytesWritten);
+                    myMemory.CloseHandle();
+                }
+                if (unlimitedHealth)
+                {
+                    
+                    myMemory.ReadProcess = myProcess[0];
+                    myMemory.Open();
+                    int pointerAddy = HexToDec(HealthPointer);
+                    //change for straight memory read/write since address is static base
+                    byte[] valueToWrite = BitConverter.GetBytes(HealthToSet);
+                    int bytesWritten;
+
+                    myMemory.Write((IntPtr)pointerAddy,valueToWrite, out bytesWritten);
+                    
+
+                    myMemory.CloseHandle();
+                }
+            }
+            else
+            {
+                isGameAvailable = false;
+                lblGameStatus.Text = "Process not found!";
+            }
+        }
+
+        public static int HexToDec(string Hex)
+        {
+            return (int.Parse(Hex, NumberStyles.HexNumber));
+        }
+        public static string DecToHex(int Dec)
+        {
+            return (Dec.ToString("X"));
+        }
+
+        private void btnHealth_Click(object sender, EventArgs e)
+        {
+
+            if (isGameAvailable)
+            {
+                if (!unlimitedHealth)
+                {
+                    unlimitedHealth = true;
+                    lblHealthStatus.Text = "Health: On";
+                }
+                else if (unlimitedHealth)
+                {
+                    unlimitedHealth = false;
+                    lblHealthStatus.Text = "Health: Off";
+                }
+            }
+
+
+        }
+    }
+}
